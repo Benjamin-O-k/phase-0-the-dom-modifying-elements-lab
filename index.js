0305059e75d2a98c6ea92ae6ed34a2cd75c46e3e
Benjamin Okello <benjamin.okello@student.moringaschool.com>
2024-07-05 19:47:44 +0300

    passed

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+let rem = document.querySelector('main#main');
+rem.remove()
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'YOUR-NAME is the champion';
